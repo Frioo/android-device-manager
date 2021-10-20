03f1dec732deb688b620be23ae2f1fe044bcd3ef
Adam Żbikowski <ajzbikowski@gmail.com>
2021-10-20 17:13:07 +0200

    Add device guard to routes (ensure that a device is connected)

--- src/App.svelte
+++ src/App.svelte
@@ -6,17 +6,13 @@
 
   /* Routing */
   import Router, { push, pop, replace } from "svelte-spa-router";
+  import wrap from "svelte-spa-router/wrap";
   import Home from "@routes/Home.svelte";
   import Explorer from "@routes/Explorer.svelte";
   import Apps from "@routes/Apps.svelte";
 
   /* JS */
-  import {
-    ls,
-    getInstalledPackages,
-    getProps,
-    dumpBattery,
-  } from "./lib/js/commands";
+  import { devices, waitForDevice } from "@js/commands";
 
   /* SMUI */
   import Drawer, {
@@ -26,11 +22,37 @@
   import List, { Item, Text } from "@smui/list/styled";
   import Button, { Label } from "@smui/button/styled";
   import Card, { Content } from "@smui/card/styled";
+  import CircularProgress from "@smui/circular-progress/styled";
+
+  let isLoading = false;
+
+  const deviceGuard = (component) => {
+    return wrap({
+      component,
+      conditions: [
+        async (detail) => {
+          isLoading = true;
+          const connectedDevices = await devices();
+          if (connectedDevices.length > 0) {
+            isLoading = false;
+            return true;
+          }
+          const device = await waitForDevice("device");
+          isLoading = false;
+          return true;
+        },
+      ],
+    });
+  };
+
+  const handleConditionsFailed = (e) => {
+    console.warn(e);
+  };
 
   const routes = {
-    "/": Home,
-    "/explorer": Explorer,
-    "/apps": Apps,
+    "/": deviceGuard(Home),
+    "/explorer": deviceGuard(Explorer),
+    "/apps": deviceGuard(Apps),
   };
 
   let items = [
@@ -63,7 +85,16 @@
     </DrawerContent>
 
     <AppContent>
-      <Router {routes} />
+      {#if isLoading}
+        <div class="loading">
+          <CircularProgress
+            style="height: 32px; width: 32px; margin-bottom: 0.5rem;"
+            indeterminate
+          />
+          <div class="mdc-typography--button">Waiting for a device...</div>
+        </div>
+      {/if}
+      <Router on:conditionsFailed={handleConditionsFailed} {routes} />
     </AppContent>
   </Drawer>
 </main>
@@ -80,5 +111,17 @@
   main {
     width: 100vw;
     height: 100vh;
+  }
+
+  .loading {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 </style>
