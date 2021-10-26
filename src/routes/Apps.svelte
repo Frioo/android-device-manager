d587f086e87374d9009a9bd57061aa60f0d085e8
Adam Żbikowski <ajzbikowski@gmail.com>
2021-10-27 01:17:16 +0200

    Apps: add filtering by system/user apps

--- src/routes/Apps.svelte
+++ src/routes/Apps.svelte
@@ -7,11 +7,38 @@
 
   /* SMUI */
   import List, { Item, Text, PrimaryText, SecondaryText } from "@smui/list";
+  import Button, { Group, Label } from "@smui/button";
+  import AppItem from "@lib/AppItem.svelte";
 
   let stats;
   let apps;
   let ubApps = apps_cache.subscribe((val) => (apps = val));
   let includeSystemApps = false;
+  let items;
+  let filters = {
+    installedBy: {
+      selected: "user",
+      values: ["user", "system", "all"],
+    },
+  };
+
+  $: if (apps) {
+    items = apps;
+  }
+
+  const setFilter = (group, value) => {
+    let newFilters = filters;
+    newFilters[group].selected = value;
+    filters = newFilters;
+    items = apps.filter((app) => {
+      return app[group] === value;
+    });
+    console.log("filters updated", items);
+  };
+
+  const onApp = (e) => {
+    statusText = `${e.current}/${e.total}`;
+  };
 
   onMount(async () => {
     //stats = await packageDiskStats();
@@ -19,32 +46,48 @@
     //await systemApps();
     await listApps({ installedBy: "all", onApp });
   });
+
+  /* const handleFilterInstalledBy = (installedBy) => {
+    items = apps.filter((app) => app.installedBy === installedBy);
+  }; */
 </script>
 
 <div class="apps">
-  {#if stats}
-    <List twoLine singleSelection>
-      {#each stats["apps"] as app}
-        <Item>
-          <Text>
-            <PrimaryText>{app.package}</PrimaryText>
-            <SecondaryText>{formatSize(app.size + app.dataSize)}</SecondaryText>
-          </Text>
-        </Item>
+  <!-- Filters -->
+  <div class="apps-filters">
+    {#each Object.keys(filters) as group}
+      <Group variant="raised">
+        {#each filters[group].values as filter}
+          <Button
+            variant="raised"
+            disabled={filters[group].selected === filter}
+            on:click={() => setFilter(group, filter)}
+          >
+            <Label>{filter}</Label>
+          </Button>
+        {/each}
+      </Group>
+    {/each}
+  </div>
+  <!-- Apps list -->
+  {#if items}
+    <div class="mdc-typography--button">
+      {statusText}
+    </div>
+    <div class="apps-list">
+      {#each items as app}
+        <AppItem {app} />
       {/each}
-    </List>
+    </div>
   {/if}
 </div>
 
-<style>
-  * :global(.mdc-deprecated-list-item__text) {
+<style lang="scss">
+  .apps-list {
     display: flex;
     flex-direction: column;
-    height: 100%;
-    justify-content: space-around;
+    width: 100%;
   }
 
-  * :global(.mdc-deprecated-list-item__primary-text) {
-    display: flex;
   }
 </style>
