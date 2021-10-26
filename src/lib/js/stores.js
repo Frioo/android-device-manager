754d9cdf8d4c7022608152489fb6cc229428d9ca
Adam Żbikowski <ajzbikowski@gmail.com>
2021-10-27 01:07:29 +0200

    Add apps cache and device info store

--- src/lib/js/stores.js
+++ src/lib/js/stores.js
@@ -1,7 +1,17 @@
 import { writable } from "svelte/store";
 
+/*  */
+export const env = writable({
+  root: true,
+});
+
+/* File explorer */
 const explorerDefault = {
   items: undefined,
   path: ["sdcard"],
 };
+
 export const explorer_cache = writable(explorerDefault);
+
+/* App manager */
+export const apps_cache = writable([]);
