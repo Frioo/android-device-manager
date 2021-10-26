2c62c19db76f797d0c2291a85c072ca771edc4fe
Adam Żbikowski <ajzbikowski@gmail.com>
2021-10-27 01:16:32 +0200

    Create AppItem.svelte

---
+++ src/lib/AppItem.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { formatSize } from "@js/utils";
+
+  /* SMUI */
+  import List, { Item, Text, PrimaryText, SecondaryText } from "@smui/list";
+  export let app;
+</script>
+
+<Item class="app">
+  <Text>
+    <PrimaryText
+      >{app?.manifest?.application?.label
+        ? app.manifest.application.label
+        : app.pkg}</PrimaryText
+    >
+    <SecondaryText>{formatSize(app.size + app.dataSize)}</SecondaryText>
+  </Text>
+</Item>
+
+<style lang="scss">
+  @import url("@styles/AppItem.scss");
+</style>
